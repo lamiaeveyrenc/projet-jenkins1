70974e605829d78c303432b52fbabcbf70ef75f3
lamiaeveyrenc <55084558+lamiaeveyrenc@users.noreply.github.com>
2020-07-24 19:59:09 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
   
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Lamiae 2020!");
+        System.out.println("Hello, Lamiae 2020 2020!");
     }
 }
